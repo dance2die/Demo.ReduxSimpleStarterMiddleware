9bf2678a8ff6e29da6c030096f96b6e3723ffab6
Sung <sungmkim80@gmail.com>
2017-05-31 19:02:52 -0400

    git push

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,15 @@
-export default function(state = null, action){
-    console.log('Action received', action);
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function (state = [], action) {
+    // console.log('Action received', action);
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // don't use state.push(action.payload.data)!!!
+            // Changing the state directly in ReDux is not a good idea
+            // as it was the case for React!
+            // return state.concat([action.payload.data]);
+            return [action.payload.data, ...state];
+    }
+
     return state;
 }
